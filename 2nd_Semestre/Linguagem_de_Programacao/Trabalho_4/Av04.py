c8acdfb0d4597503b04f7e443fc094e8d1347ee9
Fabio H. Sabino <fabiohsabino@gmail.com>
2022-06-02 16:50:06 -0300

    Adicao de comentarios trabalho 4 Renata

--- 2nd_Semestre/Linguagem_de_Programacao/Trabalho_4/Av04.py
+++ 2nd_Semestre/Linguagem_de_Programacao/Trabalho_4/Av04.py
@@ -26,10 +26,10 @@ os.system("mkdir Output")
 for i in range(100):
     fi = open("Input\\A_"+str(i+1)+".txt", "w")
     fo = open("Output\\A_"+str(i+1)+".txt", "w")
-    n = randint(1, 5)
+    n = randint(1, 5) #Quantidade de vezes que o codigo vai rodar dentro do arquivo
     fi.write(f"{n}\n")
     for i in range(n):
-        x = randint(1,15)
+        x = randint(1,15) #numero minimo e maximo que sera gerado para o collatz
         fi.write(f"{x}\n")
         fo.write(f"Valor inicial: {x}\n")
         print(f"Valor inicial: {x}")
